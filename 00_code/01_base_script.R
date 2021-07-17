bad4c40284af38883b28f567e48e107a24d6aa16
arthurlin-kmd <72428727+arthurlin-kmd@users.noreply.github.com>
2021-07-17 14:35:33 +1000

    changed x axis in ggplot

--- 00_code/01_base_script.R
+++ 00_code/01_base_script.R
@@ -34,30 +34,41 @@ daily_sales <- kmdr::base_txn_query(con, period_start = date_start, period_end =
     ungroup() %>% 
     glimpse()
 
+
 new_member_tbl <- base_member_query(con) %>% 
     filter(join_week_start >= date_start & join_week_start <= date_end & country %in% c("Australia", "New Zealand")) %>% 
     
-    #how do you perfrom left join to get customer age?
-    group_by(country, join_week_start) %>% 
+    dplyr::inner_join(dplyr::select(tbl_database$dim_customer, 
+                              dim_customer_key, research_age), by = c("dim_customer_key")) %>% 
+        
+    select(customer_number, country, join_week_start, research_age) %>% 
+    mutate(
+        research_age_recode = case_when(
+            research_age <= "0" ~ "00. Undefined",
+            research_age < "18" ~ "01. 0 - 17 yo",
+            research_age < "25" ~ "02. 18 - 24 yo",
+            research_age < "35" ~ "03. 25 - 34 yo",
+            research_age < "40" ~ "04. 35 - 39 yo",
+            research_age < "50" ~ "05. 40 - 49 yo",
+            research_age < "60" ~ "06. 50 - 59 yo",
+            research_age >= "60" ~ "07. 60+ yo"
+        )
+    ) %>%
+    group_by(country, join_week_start, research_age_recode) %>%
     summarise(
         new_members = n_distinct(customer_number)
     ) %>% 
-    collect() %>% 
-    glimpse()
+    collect()
 
-daily_sales %>% recode_dates() %>% 
-    ggplot(aes(x = full_date, y = n))+
-    geom_line() +
+
+new_member_tbl %>% recode_dates() %>% 
+    ggplot(aes(x = research_age_recode, y = new_members))+
+    geom_col() +
     scale_y_continuous(labels = scales::label_comma())+
     theme_minimal() +
     theme(plot.title.position = "plot")+
-    labs(title = "daily sc transactions",
+    labs(title = "Weekly SC acquisition",
          subtitle = glue::glue("{date_start} to {date_end}"),
-         x = "date",
-         y = "sc transactions"
+         y = "New Members"
     )
     
-members_tbl <- base_member_query(con) %>%
-    head(1000) %>% glimpse()
-    collect()
-
